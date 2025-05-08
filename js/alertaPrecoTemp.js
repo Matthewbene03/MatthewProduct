dd43a9f0371b73d105f8f7d17b326cfa3e606313
Matheus Benevenuto <matthewbene03@gmail.com>
2025-05-08 10:45:53 -0300

    Atualizações no projeto: Comentarios explicando para o que serve a paginha alertaPrecoTemp.js

--- js/alertaPrecoTemp.js
+++ js/alertaPrecoTemp.js
@@ -1,5 +1,7 @@
 // --->>>---Inicio do código JS para a pagina alertaPrecoTemp.html ---<<<---
 
+//Esse arquivo serve apenas para monitorar os preços dos produtos para a pagina alertaPreco.js
+
 import {Temporizador} from "./temporizador.js";
 
 setInterval(() => {
